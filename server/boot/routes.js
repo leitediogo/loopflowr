83f9318c9c0037ad5e0bb9294fd297a3a23bd35a
leitediogo <leitediogo@Diogos-MacBook-Pro.local>
2016-06-23 23:08:14 +0100

    loopback routes and static pages configured

--- server/boot/routes.js
+++ server/boot/routes.js
@@ -1,7 +1,15 @@
 module.exports = function(app) {
   var router = app.loopback.Router();
+
   router.get('/ping', function(req, res) {
     res.send('pongaroo');
   });
+  router.get('/start', function (req, res) {
+    res.send('start');
+  })
+  router.get('/go', function (req, res) {
+    res.send('client/index.html');
+  })
+  
   app.use(router);
 }
